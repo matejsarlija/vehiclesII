0ab7bc481468456fcca39ae4fd5d4d1a01998119
matejsarlija <matejsarlija1@gmail.com>
2023-02-21 21:07:48 +0100

    UoW, not sure how to progress

--- Vehicles.Repository/IVehicleUnitOfWork.cs
+++ Vehicles.Repository/IVehicleUnitOfWork.cs
@@ -1,6 +1,12 @@
+using Vehicles.Repository.Common;
+using Vehicles.Service.Models;
+
 namespace Vehicles.Repository;
 
 public interface IVehicleUnitOfWork
 {
-    
+    IRepository<VehicleMake> VehicleMakeRepository { get; }
+    IRepository<VehicleModel> VehicleModelRepository { get; }
+    Task<int> SaveChangesAsync();
+
 }
--- Vehicles.Repository/VehicleUnitOfWork.cs
+++ Vehicles.Repository/VehicleUnitOfWork.cs
@@ -1,6 +1,49 @@
+using Vehicles.Repository.Common;
+using Vehicles.Service.Data;
+using Vehicles.Service.Models;
+
 namespace Vehicles.Repository;
 
-public class VehicleUnitOfWork
+public class VehicleUnitOfWork : IVehicleUnitOfWork
 {
-    
+    private readonly VehicleContext _context;
+    private IRepository<VehicleMake> _vehicleMakeRepository;
+    private IRepository<VehicleModel> _vehicleModelRepository;
+
+    public VehicleUnitOfWork(VehicleContext context)
+    {
+        _context = context;
+    }
+
+
+    public IRepository<VehicleMake> VehicleMakeRepository
+    {
+        get
+        {
+            if (_vehicleMakeRepository == null)
+            {
+                _vehicleMakeRepository = new GenericRepository<VehicleMake>(_context);
+            }
+
+            return _vehicleMakeRepository;
+        }
+    }
+
+    public IRepository<VehicleModel> VehicleModelRepository
+    {
+        get
+        {
+            if (_vehicleModelRepository == null)
+            {
+                _vehicleModelRepository = new GenericRepository<VehicleModel>(_context);
+            }
+
+            return _vehicleModelRepository;
+            
+        }
+    }
+    public async Task<int> SaveChangesAsync()
+    {
+        return await _context.SaveChangesAsync();
+    }
 }
