e2549bab8dd27917e02bfea3ea1db2759a39dc05
matejsarlija <matejsarlija1@gmail.com>
2023-02-23 17:29:16 +0100

    changes to both Program.cs-es, mapping and DIs

--- Vehicles.WebAPI/Program.cs
+++ Vehicles.WebAPI/Program.cs
@@ -1,6 +1,28 @@
+using System.Configuration;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Vehicles.Model.Common.Profiles;
+using Vehicles.Repository;
+using Vehicles.Service.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<VehicleContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("VehicleContext")));
+
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+builder.Host.ConfigureContainer<ContainerBuilder>(depBuilder =>
+{
+    depBuilder.RegisterType<VehicleUnitOfWork>().As<IVehicleUnitOfWork>();
+    //depBuilder.RegisterType<VehicleModelRepository>().As<IVehicleModelRepository>();
+});
+
+
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+builder.Services.AddAutoMapper(typeof(VehiclesApiProfile));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -14,6 +36,14 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<VehicleContext>();
+    //context.Database.Migrate();
 }
 
 app.UseHttpsRedirection();
